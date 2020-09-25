1128803bae4561e467906954cfe8080d54ed8444
ksenkso <yazunya@gmail.com>
2020-09-25 23:03:24 +0300

    feature: set preferred entity

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { SET_USER_TYPE } from '@/store/action-types';
+import { SET_USER_TYPE } from '@/store/mutation-types';
+import { SET_USER_PREFERRED_ENTITY } from '@/store/mutation-types';
 
 Vue.use(Vuex);
 const USER_TYPE_KEY = 'ggtu_timetable/user_type';
+const USER_PREFERRED_ENTITY_KEY = 'ggtu_timetable/user_entity';
 export default new Vuex.Store({
   state: {
     user: {
-      type: localStorage.getItem(USER_TYPE_KEY) || ''
+      type: localStorage.getItem(USER_TYPE_KEY) || '',
+      entityId: +(localStorage.getItem(USER_PREFERRED_ENTITY_KEY) || 0)
     }
   },
   mutations: {
     [SET_USER_TYPE](state, type: string) {
       localStorage.setItem(USER_TYPE_KEY, type);
       state.user.type = type;
+    },
+    [SET_USER_PREFERRED_ENTITY](state, id: number) {
+      localStorage.setItem(USER_PREFERRED_ENTITY_KEY, id.toString());
+      state.user.entityId = id;
     }
   },
   actions: {},
--- src/views/FirstVisit.vue
+++ src/views/FirstVisit.vue
@@ -8,19 +8,61 @@
       <Button @click.native="choose('group')">Для студентов</Button>
       <Button @click.native="choose('teacher')">Для преподавателей</Button>
     </div>
+    <div class="first-visit__selector" v-if="type && !isLoading">
+      <Form @submit="submitForm">
+        <Field name="entityId" :label="listLabel">
+          <template v-slot:input="{ updateValue }">
+            <ListBox @select="updateValue" :options="options"></ListBox>
+          </template>
+        </Field>
+      </Form>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { SET_USER_TYPE } from '@/store/action-types';
+import { SET_USER_TYPE } from '@/store/mutation-types';
+import api from '@/api';
+import ListBox from '@/components/ListBox.vue';
+import Field from '@/components/Field.vue';
+import { defaultEntityAdapter } from '@/utils/lists';
+import { SelectOption } from '@/utils/lists';
+import { NamedEntity } from '@/store/types';
+import Form from '@/components/Form.vue';
+import { SET_USER_PREFERRED_ENTITY } from '@/store/mutation-types';
 
 @Component({
-  name: 'FirstVisit'
+  name: 'FirstVisit',
+  components: { ListBox, Field, Form }
 })
 export default class FirstVisit extends Vue {
+  options: SelectOption[] = [];
+  type = '';
+  isLoading = false;
+
+  get listLabel() {
+    return this.type === 'group' ? 'Выберите группу' : 'Выберите преподавателя';
+  }
+
   choose(type: 'group' | 'teacher') {
     this.$store.commit(SET_USER_TYPE, type);
+    this.type = type;
+    let request: Promise<NamedEntity[]>;
+    if (type === 'group') {
+      request = api.groups.getAll();
+    } else {
+      request = api.teachers.getAll();
+    }
+    this.isLoading = true;
+    request.then(defaultEntityAdapter).then(options => {
+      this.options = options;
+      this.isLoading = false;
+    });
+  }
+
+  submitForm(data: { entityId: number }) {
+    this.$store.commit(SET_USER_PREFERRED_ENTITY, data.entityId);
     this.$router.push('/');
   }
 }
