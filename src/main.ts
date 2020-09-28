daee584b79d63d990b2657c06dfc7422cd663de9
ksenkso <yazunya@gmail.com>
2020-09-28 11:48:12 +0300

    fix: register Alert globally

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import Button from '@/components/Button.vue';
+import Alert from '@/components/Alert.vue';
 
 Vue.config.productionTip = false;
 Vue.component('Button', Button);
+Vue.component('Alert', Alert);
 new Vue({
   router,
   store,
