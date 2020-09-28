0be12843c5931fa372c0e646534d823b2a6f904a
ksenkso <yazunya@gmail.com>
2020-09-28 16:46:33 +0300

    refactor: use separate component for the lesson card

---
+++ src/components/TimetableCard.vue
@@ -0,0 +1,88 @@
+<template>
+  <div :class="['lesson', { lesson_empty: isEmpty }]">
+    <h3>Пара {{ entry.index + 1 }}</h3>
+    <template v-if="isEmpty">
+      <div class="lesson__empty">Пары нет</div>
+    </template>
+    <template v-else>
+      <div class="lesson__name">{{ entry.lesson.name }}</div>
+      <div class="lesson__type">{{ entry.type | lessonType }}</div>
+      <div class="lesson__place">{{ entry.cabinet | lessonCabinet }}</div>
+      <div class="lesson__teachers">
+        {{ entry.teachers | joinNamedEntities }}
+      </div>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { Prop } from 'vue-property-decorator';
+import { TimetableEntry } from 'ggtu-timetable-api-client';
+import { TimetableEntryType } from 'ggtu-timetable-api-client';
+import { Cabinet } from 'ggtu-timetable-api-client';
+import { NamedEntity } from '@/store/types';
+
+@Component({
+  name: 'TimetableCard',
+  filters: {
+    lessonType(type: TimetableEntryType) {
+      switch (type) {
+        case TimetableEntryType.Lab:
+          return 'Лабораторное занятие';
+        case TimetableEntryType.Lecture:
+          return 'Лекция';
+        case TimetableEntryType.Practice:
+          return 'Практическое занятие';
+        case TimetableEntryType.Empty:
+          return 'Нет занятия';
+      }
+    },
+    joinNamedEntities(teachers: NamedEntity[]) {
+      return teachers.map(teacher => teacher.name).join(', ');
+    },
+    lessonCabinet(cabinet: Cabinet): string {
+      if (cabinet.building) {
+        return cabinet.building?.name + '\n' + cabinet.name;
+      }
+      return cabinet.name;
+    }
+  }
+})
+export default class TimetableCard extends Vue {
+  @Prop({ required: true }) entry!: TimetableEntry;
+
+  get isEmpty() {
+    return this.entry.type === TimetableEntryType.Empty;
+  }
+}
+</script>
+
+<style scoped lang="sass">
+@import "../assets/functions"
+
+.lesson
+  display: grid
+  grid-template-columns: 1fr 100px
+  grid-template-rows: repeat(auto-fit, 1fr)
+  grid-template-areas: "title title" "name place" "type place" "teachers place"
+  grid-row-gap: .5rem
+  padding: 1.5rem 1rem
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, .13)
+  &_empty
+    background-color: rgba(theme-color("warning"), .3)
+    color: darken(theme-color("warning"), 30%)
+    display: block
+  h3
+    margin-top: 0
+  &__place
+    white-space: pre-wrap
+    text-align: right
+    grid-area: place
+  &__teachers
+    grid-area: teachers
+  &__name
+    grid-area: name
+  &__type
+    grid-area: type
+</style>
--- src/views/TimetableView.vue
+++ src/views/TimetableView.vue
@@ -2,22 +2,11 @@
   <Page>
     <div class="timetable" v-if="hasLoaded">
       <div
-        class="timetable__lesson lesson"
-        v-for="(lesson, index) in currentDay"
+        class="timetable__lesson"
+        v-for="lesson in currentDay"
         :key="lesson.id"
       >
-        <h3>Пара {{ index + 1 }}</h3>
-        <Alert v-if="isEmptyLesson(lesson)" theme="warning">Пары нет</Alert>
-        <div v-else class="lesson__content">
-          <div class="lesson__name">{{ lesson.lesson.name }}</div>
-          <div class="lesson__type">{{ lesson.type | lessonType }}</div>
-          <div class="lesson__cabinet">
-            {{ lesson.cabinet | lessonCabinet }}
-          </div>
-          <div class="lesson__teachers">
-            {{ lesson.teachers | joinNamedEntities }}
-          </div>
-        </div>
+        <TimetableCard :entry="lesson"></TimetableCard>
       </div>
     </div>
   </Page>
@@ -26,40 +15,15 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { TimetableEntry } from 'ggtu-timetable-api-client';
-import { TimetableEntryType } from 'ggtu-timetable-api-client';
 import { State } from 'vuex-class';
 import Page from '@/components/Page.vue';
 import { LOAD_DEFAULT_TIMETABLE } from '@/store/action-types';
 import { EmptyTimetableEntry } from '@/utils/timetable';
-import { NamedEntity } from '@/store/types';
-import { Cabinet } from 'ggtu-timetable-api-client';
+import TimetableCard from '@/components/TimetableCard.vue';
 
 @Component({
   name: 'TimetableView',
-  components: { Page },
-  filters: {
-    lessonType(type: TimetableEntryType) {
-      switch (type) {
-        case TimetableEntryType.Lab:
-          return 'Лабораторное занятие';
-        case TimetableEntryType.Lecture:
-          return 'Лекция';
-        case TimetableEntryType.Practice:
-          return 'Практическое занятие';
-        case TimetableEntryType.Empty:
-          return 'Нет занятия';
-      }
-    },
-    joinNamedEntities(teachers: NamedEntity[]) {
-      return teachers.map(teacher => teacher.name).join(', ');
-    },
-    lessonCabinet(cabinet: Cabinet): string {
-      if (cabinet.building) {
-        return cabinet.building?.name + ', ' + cabinet.name;
-      }
-      return cabinet.name;
-    }
-  }
+  components: { Page, TimetableCard }
 })
 export default class TimetableView extends Vue {
   @State(state => state.timetable.default) timetable!: (
@@ -70,10 +34,6 @@ export default class TimetableView extends Vue {
 
   get currentDay() {
     return this.timetable[0][0];
-  }
-
-  isEmptyLesson(lesson: TimetableEntry | EmptyTimetableEntry) {
-    return lesson.type === TimetableEntryType.Empty;
   }
 
   mounted() {
@@ -90,7 +50,4 @@ export default class TimetableView extends Vue {
   display: grid
   grid-template-rows: repeat(auto-fit, 1fr)
   grid-row-gap: 1rem
-  &__lesson
-    padding: .8rem .5rem
-    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, .13)
 </style>
