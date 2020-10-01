8ee44b5826a5f661aa79766091e8b3b084c6f146
ksenkso <yazunya@gmail.com>
2020-10-01 14:46:52 +0300

    feature: vue-carousel

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import router from './router';
 import store from './store';
 import Button from '@/components/common/Button.vue';
 import Alert from '@/components/common/Alert.vue';
+// @ts-ignore
+import VueCarousel from 'vue-carousel';
 
+Vue.use(VueCarousel);
 Vue.config.productionTip = false;
 Vue.component('Button', Button);
 Vue.component('Alert', Alert);
