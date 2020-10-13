9c07c8914e983a871a68476ec29de28e1b123325
ksenkso <yazunya@gmail.com>
2020-10-13 18:16:24 +0300

    feature: show badge if provided lesson is a patch, show warning if there is no lesson at all

--- src/components/timetables/LessonView.vue
+++ src/components/timetables/LessonView.vue
@@ -1,29 +1,45 @@
 <template>
-  <Card>
-    <template slot="header"> Пара {{ lesson.index + 1 }} </template>
-    <div class="card__row">
-      <p>
-        {{ lesson.subject.name }}
-        <br />
-        {{ lesson.type | lessonType }}
-        <br />
-        {{ lesson.cabinet | cabinetName }}
-      </p>
-    </div>
-    <div class="card__row">
-      <p></p>
-    </div>
-    <div class="card__row">
-      <ul class="list">
-        <li
-          class="list__item"
-          v-for="teacher in lesson.teachers"
-          :key="teacher.id"
-        >
-          {{ teacher.name }}
-        </li>
-      </ul>
-    </div>
+  <Card
+    :theme="!lesson && 'warning'"
+    :class="[
+      'timetable__lesson',
+      { timetable__lesson_patch: this.lesson && !this.lesson.isRegular }
+    ]"
+  >
+    <template slot="header">
+      Пара {{ index + 1 }}
+      <Badge theme="primary" v-if="lesson && !lesson.isRegular"
+        >изменение</Badge
+      >
+    </template>
+    <template v-if="lesson">
+      <div class="card__row">
+        <p>
+          {{ lesson.subject.name }}
+          <br />
+          {{ lesson.type | lessonType }}
+          <br />
+          {{ lesson.cabinet | cabinetName }}
+        </p>
+      </div>
+      <div class="card__row">
+        <p></p>
+      </div>
+      <div class="card__row">
+        <ul class="list">
+          <li
+            class="list__item"
+            v-for="teacher in lesson.teachers"
+            :key="teacher.id"
+          >
+            {{ teacher.name }}
+          </li>
+        </ul>
+      </div>
+    </template>
+    <template v-else>
+      Нет пары
+    </template>
   </Card>
 </template>
 
@@ -31,17 +47,26 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { Lesson } from 'ggtu-timetable-api-client';
 import Card from '@/components/common/Card.vue';
+import { Patch } from 'ggtu-timetable-api-client';
 
 @Component({
   name: 'LessonView',
   components: { Card }
 })
 export default class LessonView extends Vue {
-  @Prop({ required: true }) lesson!: Lesson;
+  @Prop({ required: true }) lesson!: Lesson | Patch | null;
+  @Prop({ required: true }) index!: number;
 }
 </script>
 
 <style lang="sass">
+@import "../../assets/functions"
+.card__header
+  display: flex
+  align-items: center
+  .badge
+    margin-left: auto
+
 .card__footer
   display: flex
   justify-content: flex-end
