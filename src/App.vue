730bbe1037f5c7dfafe75b4eb8b93f10f7109786
ksenkso <yazunya@gmail.com>
2020-10-21 22:08:38 +0300

    feature: set default color as #222

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,17 @@
 <style lang="sass">
 #app
   height: 100%
+
 html, body
   margin: 0
   padding: 0
   height: 100%
   font-family: Tahoma, Arial, sans-serif
+  color: #222222
+
 body.menu-open
   overflow: hidden
+
 *
   box-sizing: border-box
 </style>
